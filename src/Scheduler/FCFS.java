ff55a924369c25bcd668af4a2453e3eceaf20fad
Marukos <mkoletsas@csd.auth.gr>
2021-01-10 22:41:07 +0200

    Implemented FCFS and SJF

--- src/Scheduler/FCFS.java
+++ src/Scheduler/FCFS.java
@@ -1,20 +1,36 @@
 package Scheduler;
 
+import Process.Process;
+
 public class FCFS extends Scheduler {
+
+    Process runningProcess;
 
     public FCFS() {
         /* TODO: you _may_ need to add some code here */
-
-        //test
+        runningProcess = null;
     }
 
     public void addProcess(Process p) {
         /* TODO: you need to add some code here */
+        this.processes.add(p);
     }
     
     public Process getNextProcess() {
-        /* TODO: you need to add some code here
-         * and change the return value */
-        return null;
+        /* TODO: you need to add some code here*/
+
+        if(this.processes.isEmpty()){
+            return null;
+        }
+
+        //First came, first served
+
+        if(runningProcess == null || runningProcess.getBurstTime() == 0){
+            runningProcess = this.processes.get(0);
+        }
+        return runningProcess;
+
     }
 }
+
+
--- src/Scheduler/RoundRobin.java
+++ src/Scheduler/RoundRobin.java
@@ -1,5 +1,7 @@
 package Scheduler;
 
+import Process.Process;
+
 public class RoundRobin extends Scheduler {
 
     private int quantum;
@@ -35,7 +37,7 @@ public class RoundRobin extends Scheduler {
 
         // If the quantum time of the current process has ended
         if (quantumExecutionIndex == quantum) {
-            // Move the last executed process from the from to the back of the queue
+            // Move the last executed process from the front to the back of the queue
             Process currentExecutingProcess = this.processes.get(0);
             this.processes.remove(currentExecutingProcess);
             this.processes.add(currentExecutingProcess);
